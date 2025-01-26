971cdf5fadf7ba7e0aef606127716ddbb28795be
minmin1805 <doanlyminh2005@gmail.com>
2025-01-26 01:17:33 -0500

    finish pushing the data for generating flashcards to the page

--- app/api/study-type/route.jsx
+++ app/api/study-type/route.jsx
@@ -1,6 +1,6 @@
 import { db } from "@/configs/db";
 import { CHAPTER_NOTES_TABLE, STUDY_TYPE_CONTENT_TABLE } from "@/configs/schema";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -29,5 +29,11 @@ export async function POST(req) {
         .where(eq(CHAPTER_NOTES_TABLE?.courseId, courseId));
         return NextResponse.json(notes);
 
+    }
+    else {
+        const result=await db.select().from(STUDY_TYPE_CONTENT_TABLE)
+        .where(and(eq(STUDY_TYPE_CONTENT_TABLE?.courseId, courseId),
+        eq(STUDY_TYPE_CONTENT_TABLE.type,studyType)))
+        return NextResponse.json(result[0]);
     }
 }
--- app/course/[courseId]/flashcards/page.jsx
+++ app/course/[courseId]/flashcards/page.jsx
@@ -1,15 +1,32 @@
 'use client'
+import axios from 'axios';
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Flashcards() {
 
     const {courseId}=useParams();
-    const GetFlashCards=()=> {
-        
+    const [flashCards, setFlashCards]=useState([]);
+
+    useEffect(()=> {
+        GetFlashCards();
+    },[])
+
+    const GetFlashCards=async()=> {
+        const result = await axios.post('/api/study-type', {
+            courseId:courseId,
+            studyType:'Flashcard'
+        })
+
+        setFlashCards(result?.data);
+        console.log('Flashcard', result.data);
+
     }
   return (
-    <div>Flashcards</div>
+    <div>
+        <h2 className='font-bold text-2xl'>Flashcards</h2>
+        <p>Flashcards: The Ultimate Tool to Lock in Concepts!</p>
+    </div>
   )
 }
 
