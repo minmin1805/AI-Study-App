3da2715e4fef693ce85e01d42d1087374165916e
minmin1805 <doanlyminh2005@gmail.com>
2025-01-27 00:34:54 -0500

    almost done with setting up the payment stuff

--- app/api/payment/manage-payment/route.jsx
+++ app/api/payment/manage-payment/route.jsx
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export async function POST(req) {
-    const stripe=new Stripe(process.env.STRIPE_SECRETE_KEY);
+    const stripe=new Stripe(process.env.STRIPE_SECRET_KEY);
 
    
-const returnUrl ='https://www.tubeguruji.com';
+const returnUrl ='';
 const {customerId} = await req.json();
 
 const portalSession = await stripe.billingPortal.sessions.create({
